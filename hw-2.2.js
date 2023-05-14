90e5802c6e0862da7bfcf82ac2048e5c62371cd8
v-sivozhelezov <@sivozhelezov.v@gmail.com@>
2023-05-14 14:06:34 +0300

    Исправлены замечания от 13.05.2023

--- hw-2.2.js
+++ hw-2.2.js
@@ -1,33 +1,33 @@
 // Задание 1
 let c = 10;
-alert (c);
+alert(c);
 c = 20;
-alert (c);
+alert(c);
 
 // Задание 2
-const yearIphone = (2007);
-alert (yearIphone);
+const yearIphone = 2007;
+alert(yearIphone);
 
 // Задание 3
 const creatorJS = "Брендан Эйх";
-alert (creatorJS);
+alert(creatorJS);
 
 // Задание 4
 let d = 10;
 let e = 2;
-alert (d+e);
-alert (d-e);
-alert (d*e);
-alert (d/e);
+alert(d+e);
+alert(d-e);
+alert(d*e);
+alert(d/e);
 
 // Задание 5
 let result = 2**5;
-alert (result);
+alert(result);
 
 // Задание 6
 let a = 9;
 let b = 2;
-alert (a % b);
+alert(a % b);
 
 // Задание 7
 let num = 1;
@@ -40,8 +40,8 @@ num --;
 alert(num);
 
 // Задание 8
-let age = prompt ("Сколько вам лет?");
-alert (age);
+let age = prompt("Сколько вам лет?");
+alert(age);
 
 // Задание 9.0
 const user = {
@@ -50,15 +50,15 @@ const user = {
     isAdmin: true
 };
 // Задание 9.1
-user ["city of residence"] = "Москва";
+user["city of residence"] = "Москва";
 // Задание 9.2
 user.age = 25;
 // Задание 9.3
-delete user ["city of residence"];
+delete user["city of residence"];
 // Задание 9.4
-let info = prompt ("Какую информацию хотите узнать о пользователе?")
-alert (user[info]);
+let info = prompt("Какую информацию хотите узнать о пользователе?")
+alert(user[info]);
 
 // // // Задание 10
-const name = prompt ("Как тебя зовут?");
-alert (`Привет, ${name}!`);
+const name = prompt("Как тебя зовут?");
+alert(`Привет, ${name}!`);
